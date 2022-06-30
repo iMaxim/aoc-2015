a3122d71e4bf049e5a2c6196e6172af218d04132
Maxim Prosvirkin <maxim.prosvirkin@gmail.com>
2022-07-01 01:48:50 +0300

    Minor changes to script

--- make_new_day.swift
+++ make_new_day.swift
@@ -5,7 +5,9 @@ let template = """
 import Foundation
 
 let inputPath = FileManager.default.currentDirectoryPath + "/input.txt"
+let samplePath = FileManager.default.currentDirectoryPath + "/sample.txt"
 let input = try! String.init(contentsOfFile: inputPath)
+let sample = try! String.init(contentsOfFile: samplePath)
 
 func partOne() {
 
@@ -25,14 +27,16 @@ partTwo()
 
 let directoryPath = manager.currentDirectoryPath
 let contentsOfDirectory = try! manager.contentsOfDirectory(atPath: directoryPath).compactMap { Int($0) }
-let latestDay = contentsOfDirectory.max() ?? 0
-let newDayPath = latestDay < 10 ? "0\(latestDay + 1)/" : "\(latestDay + 1)/"
+let newDay = (contentsOfDirectory.max() ?? 0) + 1
+let newDayPath = newDay < 10 ? "0\(newDay)/" : "\(newDay)/"
 
 try! manager.createDirectory(atPath: newDayPath, withIntermediateDirectories: false)
 
 let mainFile = newDayPath + "main.swift"
 let inputFile = newDayPath + "input.txt"
+let sampleFile = newDayPath + "sample.txt"
 
 let data = template.data(using: .utf8)
 manager.createFile(atPath: mainFile, contents: data)
 manager.createFile(atPath: inputFile, contents: nil)
+manager.createFile(atPath: sampleFile, contents: nil)
